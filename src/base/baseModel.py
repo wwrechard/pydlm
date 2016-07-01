369c7bb21d96e3eb7e75efeeba3d44ae3a6fbf0d
Sam <wwrechard@gmail.com>
2016-07-01 03:45:55 -0400

    finished the main part of Kalman filter

--- src/base/baseModel.py
+++ src/base/baseModel.py
@@ -4,19 +4,24 @@ import tools as tl
 
 # define the basic structure for a dlm model
 class baseModel:
-    
+            
     # define the components of a baseModel
-    def __init__(self, transition, evalution, obsVar, sysVar, innovation, state, df):
-        baseModel.transition = transition
-        baseModel.evalution = evalution
-        baseModel.obsVar = obsVar
-        baseModel.sysVar = sysVar
-        baseModel.innovation = innovation
-        baseModel.state = state
-        baseModel.df = df
+    def __init__(self, transition, evaluation, noiseVar, sysVar, innovation, state, df):
+        self.transition = transition
+        self.evaluation = evaluation
+        self.noiseVar = noiseVar
+        self.sysVar = sysVar
+        self.innovation = innovation
+        self.state = state
+        self.df = df
 
         self.validation(self)
-        baseModel.obs = np.dot(evalution, state)
+        self.obs = np.dot(evaluation, state)
+        self.obsVar = np.dot(np.dot(self.evaluation, self.sysVar), self.evalution) \
+                      + self.noiseVar
+
+        # a hidden data field used only for model prediction
+        self.prediction = self.__model__()
 
     # checking if the dimension matches with each other
     def validation(self):
@@ -33,3 +38,15 @@ class baseModel:
         tl.checker.checkVectorDimension(self.evalution, self.transition)
         tl.checker.checkVectorDimension(self.obsVar, self.evalution)
         tl.checker.checkVectorDimension(self.state, self.transition)
+
+        
+# define an inner class to store intermediate results
+class __model__:
+
+    # to store result for prediction
+    def __init__(self, step = 0, state = [], obs = [], sysVar = [], obsVar = []):
+        self.step = 0
+        self.state = state
+        self.obs = obs
+        self.sysVar = sysVar
+        self.obsVar = obsVar
--- src/base/kalmanFilter.py
+++ src/base/kalmanFilter.py
@@ -1,7 +1,6 @@
 # This code take care of the Kalman filter
 import numpy as np
 import tools as tl
-import baseModel as bm
 
 # Define the class of Kalman filter which offers a forward filter
 # backward smoother and backward sampler for one-step move
@@ -9,29 +8,121 @@ import baseModel as bm
 class kalmanFilter:
 
     # One parameter for Kalman filter
-    def __init__(self, discount):
+    def __init__(self, discount = 0.99):
         self.discount = discount
         if discount < 0 or discount > 1:
             raise tl.matrixErrors('discount factor must be between 0 and 1')
 
+    # A function used to forecast at n steps
+    def predict(self, model, step = 1):
+
+        # clear the previous prediction status
+        model.prediction.step = 0
+
+        # start predicting
+        for i in range(step):
+            self.__predict(model)
+            
     # The forward filter of one-step update given a new observation
     def forwardFilter(self, model, y):
 
+        # first obtain the predicted status
+        self.predict(model)
+        
         # when y is not a missing data
-        if y != 'na':
-
-            # predicted states and obs and their variances
-            predState = np.dot(model.transition, model.state)
-            predObs = np.dot(model.evaluation, predState)
-            predSysVar = np.dot(np.dot(model.transition, model.sysVar), \
-                                model.transition.T) + model.innovation
-            predObsInvVar = np.dot(np.dot(model.evalution, predSysVar), \
-                                model.evalution.T) + model.obsVar
-            
+        if y != 'na':    
             # the prediction error and the correction matrix
-            err = y - predObs
-            correction = np.dot(predSysVar, model.evaluation.T) / predObsInvVar
+            err = y - model.obs
+            correction = np.dot(model.prediction.sysVar, model.evaluation.T) \
+                         / model.prediciton.obsVar
 
             # update new staets
             model.df += 1
-            model.obsVar = model.obsVar + 
+            lastNoiseVar = model.noiseVar # for updating model.sysVar
+            model.noiseVar = model.noiseVar * \
+                             (1 - 1 / model.df + err^2 / model.df / model.prediction.obsVar)
+            model.state = model.state + correction * err
+            model.sysVar = model.noiseVar / lastNoiseVar * \
+                           (model.prediction.sysVar - np.dot(correction, correction.T) * \
+                            model.prediction.obsVar)
+
+            # update the innovation using discount
+            model.innovation = model.sysVar * (1 / self.discount - 1)
+
+        # when y is missing, then we update the status by the predicted results
+        else:
+            model.state = model.prediction.state
+            model.sysVar = model.prediction.sysVar
+            model.obs = model.prediction.obs
+            model.obsVar = model.prediction.obsVar
+
+
+    # The backward smoother for a given unsmoothed states at time t
+    # what model should store:
+    #      model.state: the last smoothed states (t + 1)
+    #      model.sysVar: the last smoothed system variance (t + 1)
+    #      model.transition: the transition at time t + 1
+    #      model.prediction.sysVar: the predicted system variance for time t + 1
+    #      model.prediction.state: the predicted state for time t + 1
+    #      rawState: the unsmoothed state at time t
+    #      rawSysVar: the unsmoothed system variance at time t
+    def backwardSmoother(self, model, rawState, rawSysVar):
+        backward = np.dot(np.dot(rawSysVar, model.transition.T), \
+                          np.linalg.inv(model.prediction.sysVar))
+        model.state = rawState + np.dot(backward, (model.state - model.prediction.state))
+        model.sysVar = rawSysVar + \
+                       np.dot(np.dot(backward, (model.sysVar - model.prediction.sysVar)), \
+                              backward.T)
+        model.obs = np.dot(model.evaluation, model.state)
+        model.obsVar = np.dot(np.dot(model.evaluation, model.sysVar), model.evaluation.T) +\
+                       model.noiseVar
+
+    # The backward sampler for a given unsmoothed states at time t
+    # what model should store:
+    #      model.state: the last smoothed states (t + 1)
+    #      model.sysVar: the last smoothed system variance (t + 1)
+    #      model.transition: the transition at time t + 1
+    #      model.prediction.sysVar: the predicted system variance for time t + 1
+    #      model.prediction.state: the predicted state for time t + 1
+    #      rawState: the unsmoothed state at time t
+    #      rawSysVar: the unsmoothed system variance at time t
+    def backwardSampler(self, model, rawState, rawSysVar):
+        backward = np.dot(np.dot(rawSysVar, model.transition.T), \
+                          np.linalg.inv(model.prediction.sysVar))
+        model.state = rawState + np.dot(backward, (model.state - model.prediction.state))
+        model.sysVar = rawSysVar + \
+                       np.dot(np.dot(backward, (model.sysVar - model.prediction.sysVar)), \
+                              backward.T)
+        model.state = np.matrix(np.random.multivariate_normal(model.state.A1, \
+                                                              model.sysVar)).T
+        model.obs = np.dot(model.evaluation, model.state)
+        model.obsVar = np.dot(np.dot(model.evaluation, model.sysVar), model.evaluation.T) +\
+                       model.noiseVar
+        model.obs = np.matrix(np.random.multivariate_normal(model.obs.A1, \
+                                                              model.obsVar)).T
+
+        
+    # A hiden method that does one step a head prediction
+    def __predict(self, model):
+
+        # if the step number == 0, we use result from the model state
+        if model.prediction.step == 0:
+            model.prediction.state = np.dot(model.transition, model.state)
+            model.prediciton.obs = np.dot(model.evaluation, model.state)
+            model.prediction.sysVar = np.dot(np.dot(model.transition, model.sysVar), \
+                                             model.transition.T) + model.innovation
+            model.prediction.obsVar = np.dot(np.dot(model.evaluation, model.sysVar), \
+                                             model.evaluation.T) + model.noiseVar
+            model.prediction.step = 1
+            
+        # otherwise, we use previous result to predict next time stamp 
+        else:
+            model.prediction.state = np.dot(model.transition, model.prediction.state)
+            model.prediciton.obs = np.dot(model.evaluation, model.prediction.state)
+            model.prediction.sysVar = np.dot(np.dot(model.transition, \
+                                                    model.prediction.sysVar),\
+                                             model.transition.T) + model.innovation
+            model.prediction.obsVar = np.dot(np.dot(model.evaluation, \
+                                                    model.prediction.sysVar), \
+                                             model.evaluation.T) + model.noiseVar
+            model.prediction.step += 1
