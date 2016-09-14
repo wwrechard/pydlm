3eebcaac547eec4098966264add3257a0eb3474d
Sam <wwrechard@gmail.com>
2016-09-14 11:39:16 -0700

    build to upload to pypi

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     version = '0.1.0',
     author = 'Xiangyu Wang',
     author_email = 'wwrechard@gmail.com',
-    description = ('An implementation of the Bayesian dynamic linear ' +
-      'model in Python'),
+    description = ('A python package for the Bayesian dynamic linear ' +
+      'model for time series modeling'),
     license = 'BSD',
     keywords = 'dlm bayes bayesian kalman filter smoothing dynamic model',
     url = 'http://wwrechard.github.com/PyDLM',
     packages = find_packages(),
     classifiers = [
-      'Development Status :: 3 - Alpha',
+      'Development Status :: 4 - Beta',
       'Intended Audience :: Science/Research',
       'License :: OSI Approved :: BSD License',
       'Operating System :: OS Independent',
