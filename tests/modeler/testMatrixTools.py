a4e47652a82875a47c1cb4f3d04d8821412e40e8
wwrechard <wwrechard@gmail.com>
2024-08-29 23:19:36 -0700

    add tests for matrix tools

--- tests/modeler/testMatrixTools.py
+++ tests/modeler/testMatrixTools.py
@@ -1,4 +1,4 @@
-import pydlm
+import numpy as np
 import unittest
 
 from pydlm.modeler.matrixTools import matrixTools as mt
@@ -6,8 +6,27 @@ from pydlm.modeler.matrixTools import matrixTools as mt
 class testMatrixTools(unittest.TestCase):
 
     def testMatrixAddByRow(self):
-        pass
+        A = np.array([[1], [2]])
+        B = np.array([[3]])
+        expected = np.array([[1], [2], [3]])
+        np.testing.assert_array_equal(mt.matrixAddByRow(A, B), expected)
 
+        A = np.array([1, 2])
+        B = np.array([3])
+        expected = np.array([1, 2, 3])
+        np.testing.assert_array_equal(mt.matrixAddByRow(A, B), expected)
 
+    def testMatrixAddByCol(self):
+        A = np.array([[1]])
+        B = np.array([[3]])
+        expected = np.array([[1, 3]])
+        np.testing.assert_array_equal(mt.matrixAddByCol(A, B), expected)
+
+        A = np.array([[1], [2]])
+        B = np.array([[3], [4]])
+        expected = np.array([[1, 3], [2, 4]])
+        np.testing.assert_array_equal(mt.matrixAddByCol(A, B), expected)
+
+        
 if __name__ == '__main__':
     unittest.main()
